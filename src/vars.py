6f2eba7ada6f27820973a718d3f4fe9a75219827
Anton Kozyriev <sonara4mus@gmail.com>
2024-01-18 01:54:49 +0200

    chore(var.py): format file

--- src/vars.py
+++ src/vars.py
@@ -5,7 +5,11 @@ DEFAULT_INIT_LIVES = 5
 DEFAULT_FACTS_NUM = 5
 DEFAULT_OPTIONS_NUM = 4
 
-COUNTRY_NAMES_FILE_LOCATION = os.getenv("VAR_COUNTRY_NAMES_FILE_LOCATION") or "./data/dev/names.json"
-COUNTRY_FACTS_FILE_LOCATION = os.getenv("VAR_COUNTRY_FACTS_FILE_LOCATION") or "./data/dev/facts.json"
+COUNTRY_NAMES_FILE_LOCATION = (
+    os.getenv("VAR_COUNTRY_NAMES_FILE_LOCATION") or "./data/dev/names.json"
+)
+COUNTRY_FACTS_FILE_LOCATION = (
+    os.getenv("VAR_COUNTRY_FACTS_FILE_LOCATION") or "./data/dev/facts.json"
+)
 
 TOKEN = os.getenv("VAR_TOKEN") or ""
